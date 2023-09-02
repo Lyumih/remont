cb0a8c19a9e678e53c1b4e2c7a81830e53078ffa
Lyumih <32732179+Lyumih@users.noreply.github.com>
2023-09-02 10:52:56 +0000

    Deploying to gh-pages from @ Lyumih/remont@01a0ecc5b0267c33681d7a9c2041679154288370 🚀

--- shops/-css/shops.view.css.ts
+++
@@ -1,3 +0,0 @@
-namespace $ { $mol_style_attach( "remont/market/shops/shops.view.css",
- "[remont_market_shops_shops] {\n\twidth: 100%;\n\tflex-wrap: wrap;\n}\n\n[remont_market_find_main_tools] {\n\tdisplay: none;\n}\n"
-) }
