22b1b4f19885bb9fb576ca1403dc995a15bdd894
Lyumih <Lyumih@users.noreply.github.com>
2024-01-09 07:16:52 +0000

    Deploying to gh-pages from @ Lyumih/remont@9677f215c2d510507f37b9c76e8367ecd78c2af4 🚀

--- node.mjs
+++ node.mjs
@@ -9349,6 +9349,7 @@ var $;
                         : this.symbols_alt()[$mol_keyboard_code[event.keyCode]];
                 if (!symbol)
                     return;
+                event.preventDefault();
                 document.execCommand('insertText', false, symbol);
             }
             clickable(next) {
@@ -9373,8 +9374,8 @@ var $;
                             break;
                         default: return;
                     }
+                    event.preventDefault();
                 }
-                event.preventDefault();
             }
             row_numb(index) {
                 return index;
